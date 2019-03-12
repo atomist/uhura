f7344107fb9d4b1efc30e4a859a91f69d70b44c3
Christian Dupuis <cd@atomist.com>
2019-03-12 10:43:02 +0100

    Delint

--- lib/generate/universal/SeedDrivenCommandParams.ts
+++ lib/generate/universal/SeedDrivenCommandParams.ts
@@ -80,5 +80,5 @@ export const OptionalSeedParamsDefinitions: ParametersObject<Pick<SeedDrivenComm
 };
 
 function isValidSHA1(s: string): boolean {
-    return s.match(/[a-fA-F0-9]{40}/) != null;
+    return s.match(/[a-fA-F0-9]{40}/) !== null;
 }
